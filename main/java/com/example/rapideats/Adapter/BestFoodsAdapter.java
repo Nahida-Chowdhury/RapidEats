f17cbdeaa87cfa04d7c4b636e072da33e1d947d7
Nahida-Chowdhury <123573394+Nahida-Chowdhury@users.noreply.github.com>
2024-06-26 14:02:14 +0600

    Add files via upload

--- main/java/com/example/rapideats/Adapter/BestFoodsAdapter.java
+++ main/java/com/example/rapideats/Adapter/BestFoodsAdapter.java
@@ -2,8 +2,11 @@ package com.example.rapideats.Adapter;
 
 import android.content.Context;
 import android.view.LayoutInflater;
+import android.view.RoundedCorner;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -26,23 +29,39 @@ public class BestFoodsAdapter extends RecyclerView.Adapter<BestFoodsAdapter.view
     @Override
     public BestFoodsAdapter.viewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         context = parent.getContext();
-        View inflate = LayoutInflater.from(parent.getContext()).inflate(R.layout);
-        return null;
+        View inflate = LayoutInflater.from(parent.getContext()).inflate(R.layout.viewholder_best_deal,parent,false);
+        return new viewholder(inflate);
     }
 
     @Override
     public void onBindViewHolder(@NonNull BestFoodsAdapter.viewholder holder, int position) {
+        holder.titleTxt.setText(items.get(position).getTitle());
+        holder.priceTxt.setText("$i"+items.get(position).getPrice());
+        holder.timeTxt.setText(items.get(position).getTimeValue()+"min");
+        holder.starTxt.setText(""+items.get(position).getStar());
 
+        Glide.with(context)
+                .load(items.get(position).getImagePath())
+                .transform(new CenterCrop(),new RoundedCorners(30))
+                .into(holder.pic);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return items.size();
     }
 
     public class viewholder extends RecyclerView.ViewHolder{
+        TextView titleTxt,priceTxt,starTxt,timeTxt;
+        ImageView pic;
         public viewholder(@NonNull View itemView) {
+
             super(itemView);
+            titleTxt = itemView.findViewById(R.id.titleTxt);
+            priceTxt = itemView.findViewById(R.id.priceTxt);
+            starTxt = itemView.findViewById(R.id.starTxt);
+            timeTxt = itemView.findViewById(R.id.timeTxt);
+            pic = itemView.findViewById(R.id.pic);
         }
     }
 }
